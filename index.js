86f74b8a83960b0d14cb1e6d592c25b38d748ad6
Kyle Hong <kyle.hong@z-emotion.com>
2025-06-29 14:21:26 +0900

    Break 'Hard Fiber / Span' value into array

--- index.js
+++ index.js
@@ -26,6 +26,20 @@ for (let i = 2; i < process.argv.length; i++) {
                 if (!fileContent.icons.find(x => normalizeTechnicalDetails(x) === normalizeTechnicalDetails(detail)))
                     console.error(`Unknown technical detail type: '${detail}' (#: ${fabric['#']}) in ${sheet} of ${filename}`);
             }
+            // Break "Hard Fiber / Span" values.
+            const hardFiberSpan = fabric['Hard Fiber / Span'];
+            if (typeof(hardFiberSpan) === 'string') {
+                const lines = hardFiberSpan.split('\r\n').map(l => l.trim());
+                const newHardFiberSpan = [lines[0]];
+                for (let i = 1; i < lines.length; ++i) {
+                    if (lines[i].match(/^[a-z] *: */i)) {
+                        newHardFiberSpan.push(lines[i]);
+                    } else {
+                        newHardFiberSpan[newHardFiberSpan.length - 1] = newHardFiberSpan[newHardFiberSpan.length - 1] + lines[i];
+                    }
+                }
+                fabric['Hard Fiber / Span'] = newHardFiberSpan;
+            }
             // Break 'Product Features' values.
             const productFeatures = fabric['Product Features'];
             if (typeof(productFeatures) === 'string') {
